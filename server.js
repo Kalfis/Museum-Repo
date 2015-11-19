b560f4050c47c1a83172a10549e326e1e8b3b821
William Chin <william-chin@outlook.com>
2015-11-19 17:55:25 -0500

    artists showing

--- server.js
+++ server.js
@@ -3,9 +3,39 @@ let express = require('express');
 let path = require('path');
 let logger = require('morgan');
 let bodyParser = require('body-parser');
-
+let Artist = require('./models/artist');
 let app = express();
 
+app.use(bodyParser.urlencoded({ extended: false }));
+let mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/museumrApp');
+// begin routes
+
+app.get('/artists', (req, res) => {
+  console.log('hit /artists');
+  res.send(Artist.Artist)
+});
+
+app.post('/create-artist', (req, res, next) => {
+    let newArtist = new Artist(req.body);
+    newArtist.save((err) => {
+      if (err) throw err;
+      console.log('New artist created!');
+    });
+    res.send(newArtist);
+  });
+// get ID
+app.get('/artists/:name', (req, res) => {
+
+    Artist.find({name: req.params.name}, function(err, result) {
+      console.log(result)
+      res.send(result);
+    })
+});
+//
+// app.put
+//
+// app.delete
 
 
 let server = app.listen(3000, () => {
