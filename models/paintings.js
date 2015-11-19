8fa0a8314b5b92a9bf488fe5f44a0ccca514a3ce
Adnan Rokadia <Adnan@Adnans-MacBook-Pro.local>
2015-11-19 12:27:13 -0500

    paintings model

---
+++ models/paintings.js
@@ -0,0 +1,11 @@
+let mongoose = require('mongoose');
+
+let paintingSchema = new mongoose.Schema({
+  title: String,
+  img_url: String,
+  year_made: Number
+});
+
+let Painting = mongoose.model('Painting', paintingSchema);
+
+module.exports = Painting;
